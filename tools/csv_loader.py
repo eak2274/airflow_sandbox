d85fcec46eb8334b72edf9f5c11a8624fad1c344
Evgeny Kosterin <sparepart74@gmail.com>
2024-10-29 11:33:32 +0200

    Correct date import in the loader

--- tools/csv_loader.py
+++ tools/csv_loader.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sqlalchemy import create_engine
+from sqlalchemy.types import Date
 
 engine = create_engine(
     "{dialect}+{driver}://{username}:{password}@{host}:{port}/{database}".format(
@@ -17,11 +18,17 @@ engine = create_engine(
 csv_file_path = "../data/Orders.csv"
 df = pd.read_csv(csv_file_path)
 
+# Задаем тип данных для столбца с датой
+dtype_mapping = {
+    'date': Date  # замените 'date_column' на название столбца в вашем DataFrame
+}
+
+
 with engine.begin() as connection:
     df.to_sql(
         name='orders',
         con=connection,
         if_exists='replace',
         index=False,
+        dtype=dtype_mapping,
     )
-
